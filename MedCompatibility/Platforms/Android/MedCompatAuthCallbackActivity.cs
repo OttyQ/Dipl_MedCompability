c2d032de33369d75131171bb7e5e98e5c536b1e6
Eugene <eugene.hatskevich25@mail.ru>
2025-12-16 00:06:36 +0300

    Login page redisign + android google service start

---
+++ MedCompatibility/Platforms/Android/MedCompatAuthCallbackActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Microsoft.Maui.Authentication;
+
+namespace MedCompatibility;
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+[IntentFilter(
+    new[] { Intent.ActionView },
+    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+    DataScheme = "medcompat",
+    DataHost = "auth")]
+public class MedCompatAuthCallbackActivity : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
+{
+}
